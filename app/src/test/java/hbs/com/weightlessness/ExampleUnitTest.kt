ed260931e5089f2288b4179df63af301a9116bc7
hakzzang <mbh930312@naver.com>
2019-08-12 13:23:24 +0900

    GetJobCafe TestCode 작성 및 Gradle 종속성 추가

--- app/src/test/java/hbs/com/weightlessness/ExampleUnitTest.kt
+++ app/src/test/java/hbs/com/weightlessness/ExampleUnitTest.kt
@@ -1,8 +1,15 @@
 package hbs.com.weightlessness
 
+import android.util.Log
+import hbs.com.weightlessness.remote.JobCafeRepositoryImpl
+import hbs.com.weightlessness.remote.RetrofitProvider
+import hbs.com.weightlessness.usecase.JobCafeUseCase
+import hbs.com.weightlessness.usecase.JobCafeUseCaseImpl
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.mockito.Mockito
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +17,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private lateinit var jobCafeUseCase: JobCafeUseCase
+    @Before
+    fun setupTest() {
+        jobCafeUseCase = JobCafeUseCaseImpl(
+            JobCafeRepositoryImpl(
+                RetrofitProvider.provideSeoulApi()
+            )
+        )
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun getJobCafe() {
+        jobCafeUseCase
+            .getJobCafeList()
+            .subscribe { wrappingJobCafeList ->
+                println(wrappingJobCafeList.jobCafeList.jobCafes.toString())
+            }
     }
 }
