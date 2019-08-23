ffbc8178c6373978d8172ec0e53512d559e39591
hakzzang <mbh930312@naver.com>
2019-08-23 17:59:47 +0900

    Sample Profile 추가 및 다이얼로그 아이콘 변경

--- app/src/main/java/hbs/com/weightlessness/ui/chat/ChattingAdapter.kt
+++ app/src/main/java/hbs/com/weightlessness/ui/chat/ChattingAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import hbs.com.weightlessness.R
 import kotlinx.android.synthetic.main.item_chatting_dialog.view.*
 
@@ -18,6 +20,11 @@ class ChattingAdapter(private val chattingList: List<String>) : RecyclerView.Ada
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val chattingViewHolder = holder as ChattingViewHolder
         chattingViewHolder.itemView.tv_chat_dialog.text = chattingList[position]
+        Glide
+            .with(chattingViewHolder.itemView.iv_profile_image)
+            .load(R.drawable.shrimp_burger)
+            .apply(RequestOptions.circleCropTransform())
+            .into(chattingViewHolder.itemView.iv_profile_image)
     }
 
     inner class ChattingViewHolder(view: View) : RecyclerView.ViewHolder(view)
